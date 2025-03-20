cd363c89f6d2dbbdfd7dcb34b5b3a8f269c07311
hojang <26dev@naver.com>
2025-03-20 13:20:10 +0900

    Initial commit

---
+++ src/test/java/org/example/youtubeaisummary/YoutubeSummarizeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.youtubeaisummary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeSummarizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
